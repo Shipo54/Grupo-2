f07a1b382b5b2cda59b5d3c66560e60aab7e4bd6
Averanga <maverangap@fcpn.edu.bo>
2025-09-03 11:31:53 -0400

    mi archivo python

---
+++ operacion.py
@@ -0,0 +1,9 @@
+#Operacion simple
+numero1 = 10
+numero2 = 5
+multiplicacion = numero1 * numero2;
+print("La multiplicacion de; ", numero1, "y", numero2, "es:", multiplicacion)
+print("1. Arias Acarapi Jose Andres")
+print("2. Averanga peña Mauro Esteban")
+print("3. Carballo Valenzuela Anneth Gricel")
+print("4. Condori Chambi Haydee") 
