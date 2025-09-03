250683be1083d21ee6ffdfa6607fa47a8d01e32b
Yeider <haydercondori1@gmail.com>
2025-09-03 10:58:07 -0400

    mi archibo saludo java

---
+++ Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, equipo!");
+        System.out.println("1 Condori Chambi Hayder");
+        System.out.println("2 Averanga Peña Maure Esteban");
+        System.out.println("3 Carballo Valenzuela Annteth Gricel ");
+        System.out.println("4 Arias Acarapi Jose Andres");
+    }
+}
